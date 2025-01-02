717a4723339c79ecc1725e7bb0a3a05242e3a1bb
Md. Mamun Hossain <muhammed.mamun.131@gmail.com>
2025-01-02 09:46:53 +0600

    arrays

---
+++ 8_arrays/main.go
@@ -0,0 +1,34 @@
+package main
+
+// numbered sequence of specific length
+func main() {
+
+	//empty array initialized with zeroed values
+	//int -> 0, string -> "", bool -> false
+
+	// var nums [5]int
+
+	//array length
+	//fmt.Println(len(nums))
+
+	// nums[0] = 1
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+
+	// var vals [5]bool
+	// fmt.Println(vals)
+
+	//to declare array in single line with values
+	// nums := [3]int{1, 2, 3}
+
+	// fmt.Println(nums)
+
+	// 2D arrays
+	// nums := [2][2]int{{3, 4}, {4, 9}}
+
+	// fmt.Println(nums)
+
+	//-> fixed size, that is predictable
+	//-> Memory Optimization
+	//-> Constant time data access
+}
