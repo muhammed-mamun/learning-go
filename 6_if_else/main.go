aa54f7f74e160c6d80783d08cef98bfb788d7183
Md. Mamun Hossain <muhammed.mamun.131@gmail.com>
2025-01-02 09:47:52 +0600

    if else

---
+++ 6_if_else/main.go
@@ -0,0 +1,33 @@
+package main
+
+func main() {
+	// age := 10
+
+	// if age >= 18 {
+	// 	fmt.Println("you are adult , now you can marry")
+	// } else {
+	// 	fmt.Println("funk you are u")
+	// }
+
+	// if age >= 18 {
+	// 	fmt.Println("you are adult , now you can marry")
+	// } else if age >= 12 {
+	// 	fmt.Println("you are teenager , you have to wait")
+	// } else {
+	// 	fmt.Println("You are a kid")
+	// }
+
+	// var role = "admin"
+	// var hasPermission = true
+
+	// if role == "admin" && hasPermission {
+	// 	fmt.Println("yes")
+	// }
+
+	//varibale declare in - if
+	// if age := 15; age >= 18 {
+	// 	fmt.Println("person isi an adult")
+	// } else {
+	// 	fmt.Println("funk !!!! who u are")
+	// }
+}
